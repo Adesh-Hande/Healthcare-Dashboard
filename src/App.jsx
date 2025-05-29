b15d6598d18caeee13f9909e52f811ee49c5a76d
Adesh Hande <adesh9362@gmail.com>
2025-05-29 17:14:10 +0530

    fix all error

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,16 @@ function App() {
 
   return (
     <>
-      <div className='main' style={{ width: "100%", margin: "auto", }}>
-        <div style={{ display: "flex" }} >
+      <div id="app-main" className='main'>
+        <div id="app-main-div" >
           <Sidebar />
           <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", width: "100%" }}>
             <Header />
             <div className="dash-board-containt" style={{ display: "flex" }}>
+              <div>
               <DashboardMainContent />
-              <div className='caledar-box' style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+              </div>
+              <div className='caledar-box' style={{  }}>
                 <Calendarview />
                 <UpcomingSchedule />
               </div>
